225adc91785737e4af40af2f697f86eebd19ced0
Zach <100164907@ccisd.net>
2025-12-14 16:31:46 -0600

    add support for os py lib
    Added a new function to generate C++ code for executing system commands.

--- include/pystdlib.hpp
+++ include/pystdlib.hpp
@@ -13,12 +13,26 @@ std::string Generate_Cpp_Print_Code(const std::string& content_to_print) {
     return cpp_code;
 }
 
+std::string Generate_Cpp_OS_Code(const std::string& command_to_execute) {
+    std::string cpp_code = "system(\"" + command_to_execute + "\");";
+    return cpp_code;
+}
+
 // Function to check if a string starts with "print("
 bool startsWithPrint(const std::string& input_string) {
     const std::string print_prefix = "print(";
 
     // Check if the prefix is found exactly at the beginning (index 0)
     if (input_string.find(print_prefix) == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool includesos(const std::string& input_string) {
+    const std::string os_prefix = "os.system(";
+
+    if (input_string.find(os_prefix) == 0) {
         return true;
     }
     return false;
